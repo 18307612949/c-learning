0c9b796e89bdebdded4e749fa002ec7880ccf700
ljl86400 <510758767@qq.com>
2018-05-08 17:15:44 +0800

    init repository

---
+++ Makefile
@@ -0,0 +1,3 @@
+subsystem:
+	cd defaults_cpp && $(MAKE)
+	cd write_cpp && $(MAKE)
---
+++ defaults_cpp/defaults.cpp
@@ -0,0 +1,17 @@
+// default.cpp -- cout default format
+#include"iostream"
+
+int main()
+{
+	using std::cout;
+	cout << "1234567890\n";
+
+	char ch = 'k';
+	int t = 273;
+	cout << ch <<":\n";
+	cout << t <<":\n";
+	cout << -t <<":\n";
+
+	return 0;
+}
+
